846bc7a4399f0746dad7cd1dd2cf11c2de0daaf0
Josh Carter <public@joshcarter.com>
2010-12-21 20:58:43 -0700

    Adding vim and rvm, couple other updates
    - Adding .vim and .vimrc
    
    - Taking out Clojure mode from emacs, not using it enough.
    
    - Updated install script.

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ cd $HOME
 ln -s $SCRIPT_DIR/emacs/dotemacs .emacs
 ln -s $SCRIPT_DIR/bash/dotbashrc .bashrc
 ln -s $SCRIPT_DIR/bash/dotbash_profile .bash_profile
+ln -s $SCRIPT_DIR/vim/dotvimrc .vimrc
+ln -s $SCRIPT_DIR/vim/dotvim .vim
