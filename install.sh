7f22e10969d6c03c6ab59ef2765f8b1648275b9a
Josh Carter <public@joshcarter.com>
2011-07-08 15:12:47 -0600

    Adding .ctags, .vim

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd)
 
 cd $HOME
+ln -s $SCRIPT_DIR/ctags/dotctags .ctags
 ln -s $SCRIPT_DIR/emacs/dotemacs .emacs
 ln -s $SCRIPT_DIR/bash/dotbashrc .bashrc
 ln -s $SCRIPT_DIR/bash/dotbash_profile .bash_profile
