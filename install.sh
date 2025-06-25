42c4ef9ee499daf4c5cd5009369220ce8efa21c6
Josh Carter <public@joshcarter.com>
2025-06-25 14:29:50 -0600

    updates for zsh and starship

--- install.sh
+++ install.sh
@@ -9,3 +9,6 @@ ln -s $SCRIPT_DIR/bash/dotbashrc .bashrc
 ln -s $SCRIPT_DIR/bash/dotbash_profile .bash_profile
 ln -s $SCRIPT_DIR/vim/dotvimrc .vimrc
 ln -s $SCRIPT_DIR/vim/dotvim .vim
+ln -s $SCRIPT_DIR/zsh/dotzprofile .zprofile
+ln -s $SCRIPT_DIR/zsh/dotzshrc .zshrc
+ln -s $SCRIPT_DIR/starship/starship.toml .starship.toml
